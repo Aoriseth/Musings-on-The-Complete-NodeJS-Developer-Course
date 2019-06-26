a54afbbffda747613de5846e2774c59582845389
Lennart Cockx <lennart.cockx@gmail.com>
2019-06-26 09:47:21 +0200

    Updated package.json and app.js for heroku deployment

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,7 @@ const geocode = require("./utils/geocode");
 const viewsPath = path.join(__dirname,"../templates/views");
 const partialsPath = path.join(__dirname,"../templates/partials");
 
+const port = process.env.PORT || 3000;
 const app = express();
 app.set("view engine","hbs");
 app.set("views",viewsPath);
@@ -86,6 +87,6 @@ app.get("*",(req,res)=>{
 
 
 
-app.listen(3000,() => {
-	console.log("Server started on port 3000")
+app.listen(port,() => {
+	console.log("Server started on port " + port)
 });
